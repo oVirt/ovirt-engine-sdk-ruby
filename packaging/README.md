ce2552d5c242b01c5d99c44f513688741d463bbc
Juan Hernandez <juan.hernandez@redhat.com>
2016-01-13 20:11:17 +0100

    Add RPM spec file
    This patch adds an RPM .spec file that can be used to build a RPM
    package for the SDK.
    
    Change-Id: I61abdb7e04f053d1c435b4ba179e40c5dacbb7c2
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ packaging/README.md
@@ -0,0 +1,11 @@
+# oVirt Ruby SDK Packaging
+
+## Introduction
+
+This directory contains the files required to package the SDK for
+packaging systems other than the Ruby gem packaging system.
+
+## RPM
+
+To build the RPM simply run the `build.sh` script. It will download the
+`.gem` file from `rubygems.org` and then will package it.
---
+++ packaging/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+# Name and version of the package:
+gem_version="${gem_version:=4.0.0.alpha0}"
+gem_url="${gem_url:=https://rubygems.org/downloads/ovirt-engine-sdk-${gem_version}.gem}"
+rpm_version="${rpm_version:=4.0.0}"
+rpm_dist="${rpm_dist:=$(rpm --eval '%dist')}"
+rpm_release="${rpm_release:=0.0${rpm_dist}}"
+
+# Generate the .spec file from the template for the distribution where the
+# build process is running:
+spec_template="spec${rpm_dist}.in"
+spec_file="rubygem-ovirt-engine-sdk.spec"
+sed \
+  -e "s|@GEM_VERSION@|${gem_version}|g" \
+  -e "s|@GEM_URL@|${gem_url}|g" \
+  -e "s|@RPM_VERSION@|${rpm_version}|g" \
+  -e "s|@RPM_RELEASE@|${rpm_release}|g" \
+  < "${spec_template}" \
+  > "${spec_file}" \
+
+# Download the sources:
+spectool \
+  --get-files \
+  "${spec_file}"
+
+# Build the source and binary .rpm files:
+rpmbuild \
+  -bb \
+  --define="_sourcedir ${PWD}" \
+  --define="_rpmdir ${PWD}" \
+  "${spec_file}"
