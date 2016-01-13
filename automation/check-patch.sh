764948570da66510eb0fe3822977adb749dbf467
Juan Hernandez <juan.hernandez@redhat.com>
2016-01-13 18:14:35 +0100

    Run unit tests for patches
    This patch changes the "check-patch.sh" so that the code generator is
    built, and executed and then the unit tests are executed.
    
    Change-Id: I70afcc8ef94fe8356153d06ad66cce1e7738a0b8
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,3 +1,32 @@
 #!/bin/sh -ex
 
-# Nothing yet.
+# Create a settings file that uses the our artifactory server as
+# proxy for all repositories:
+settings="$(pwd)/settings.xml"
+cat > "${settings}" <<.
+<settings>
+  <mirrors>
+
+    <mirror>
+      <id>ovirt-artifactory</id>
+      <url>http://artifactory.ovirt.org/artifactory/ovirt-mirror</url>
+      <mirrorOf>*</mirrorOf>
+    </mirror>
+
+    <mirror>
+      <id>maven-central</id>
+      <url>http://repo.maven.apache.org/maven2</url>
+      <mirrorOf>*</mirrorOf>
+    </mirror>
+
+  </mirrors>
+</settings>
+.
+
+# There may be several versions of Java installed in the build
+# enviroment, and we need to make sure that Java 8 is used, as
+# it is required by the code generator:
+export JAVA_HOME="${JAVA_HOME:=/usr/lib/jvm/java-1.8.0}"
+
+# Build and run the tests:
+mvn test -s "${settings}"
