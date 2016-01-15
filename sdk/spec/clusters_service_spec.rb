60c1ceca658fc4f0966c84a19092269a8b04b53f
Juan Hernandez <juan.hernandez@redhat.com>
2016-01-15 18:14:19 +0100

    Add initial tests for relevant services
    This patch adds some simple tests for the services that are more
    relevant, like the data centers service, the clusters service and the
    storage domains service.
    
    Change-Id: I867045e7df814e6108fc610d757cb67a3b46b88f
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ sdk/spec/clusters_service_spec.rb
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2015 Red Hat, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe SDK::ClustersService do
+
+  before(:each) do
+    @connection = default_connection
+    @clusters = @connection.system.clusters
+  end
+
+  after(:each) do
+    @connection.close
+  end
+
+  describe ".clusters", :integration => true do
+
+    context "getting the reference to the service" do
+
+      it "doesn't return nil" do
+        expect(@clusters).not_to be_nil
+      end
+
+    end
+
+  end
+
+  describe ".list", :integration => true do
+
+    context "without parameters" do
+
+      it "returns a list, maybe empty" do
+        clusters = @clusters.list
+        expect(clusters).not_to be_nil
+        expect(clusters).to be_an(Array)
+      end
+
+    end
+
+    context "with an unfeasible query" do
+
+      it "returns an empty array" do
+        clusters = @clusters.list({:search => 'name=ugly'})
+        expect(clusters).to eql([])
+      end
+
+    end
+
+  end
+
+end
---
+++ sdk/spec/data_centers_service_spec.rb
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2015 Red Hat, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe SDK::DataCentersService do
+
+  before(:each) do
+    @connection = default_connection
+    @dcs = @connection.system.data_centers
+  end
+
+  after(:each) do
+    @connection.close
+  end
+
+  describe ".data_centers", :integration => true do
+
+    context "getting the reference to the service" do
+
+      it "doesn't return nil" do
+        expect(@dcs).not_to be_nil
+      end
+
+    end
+
+  end
+
+  describe ".list", :integration => true do
+
+    context "without parameters" do
+
+      it "returns a list, maybe empty" do
+        dcs = @dcs.list
+        expect(dcs).not_to be_nil
+        expect(dcs).to be_an(Array)
+      end
+
+    end
+
+    context "with an unfeasible query" do
+
+      it "returns an empty array" do
+        dcs = @dcs.list({:search => 'name=ugly'})
+        expect(dcs).to eql([])
+      end
+
+    end
+
+  end
+
+end
