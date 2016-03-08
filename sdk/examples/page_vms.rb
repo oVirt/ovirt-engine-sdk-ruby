0d3e6625e99f8879d68bbfbe47138b7a2967c7e0
Juan Hernandez <juan.hernandez@redhat.com>
2016-03-08 19:41:46 +0100

    Add example of paging
    This patch adds an example that shows how to use the "max" parameter and
    the "page" query syntax to iterate a collection using paging.
    
    Change-Id: I15d63c94ff56c4004a7afd4abd0369f0d1e83f87
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ sdk/examples/page_vms.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/ruby
+
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'ovirtsdk4'
+
+# This example will connect to the server and list the virtual machines
+# using paging:
+
+# Create the connection to the server:
+connection = OvirtSDK4::Connection.new({
+  :url => 'https://engine40.example.com/ovirt-engine/api',
+  :username => 'admin@internal',
+  :password => 'redhat123',
+  :ca_file => 'ca.pem',
+  :debug => true,
+})
+
+# Get the reference to the "vms" service:
+vms_service = connection.system_service.vms_service
+
+# Use the "list" method of the "vms" service to list all the virtual machines of the system:
+
+# List the virtual machines, page by page:
+size = 1
+index = 1
+loop do
+  page = vms_service.list({
+    :search => "page #{index}",
+    :max => size,
+  })
+  break unless page.length > 0
+  page.each do |vm|
+    puts("#{vm.name}")
+  end
+  index += 1
+end
+
+# Close the connection to the server:
+connection.close
