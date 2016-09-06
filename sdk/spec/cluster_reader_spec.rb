e1a08652707d2359fcd38833d9f86b1800d652c4
Juan Hernandez <juan.hernandez@redhat.com>
2016-09-06 10:50:25 +0200

    Fix reading of lists of elements
    Currently the method that read lists of elements doesn't handle
    correctly empty elements without close tags. For example, when reading
    the following cluster:
    
      <cluster>
        ...
        <required_rng_sources/>
        <switch_type>legacy</switch_type>
      </cluster>
    
    The 'legacy' value of the 'switch_type' element is interpreted as one of
    the values of the previous 'required_rng_sources' list. This patch fixes
    the method and introduces two tests to verify that it works correctly.
    
    Change-Id: Id1495e873642aadd09ba3dd578077346f17e873e
    Bug-Url: https://bugzilla.redhat.com/1373436
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ sdk/spec/cluster_reader_spec.rb
@@ -0,0 +1,42 @@
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+describe SDK::ClusterReader do
+
+  describe '#read_one' do
+
+    context 'given switch type after empty RNG sources' do
+
+      it 'both are read correctly' do
+        reader = SDK::XmlReader.new(
+          '<cluster>' +
+            '<required_rng_sources/>' +
+            '<switch_type>legacy</switch_type>' +
+          '</cluster>'
+        )
+        result = SDK::ClusterReader.read_one(reader)
+        reader.close
+        expect(result).to_not be_nil
+        expect(result).to be_a(SDK::Cluster)
+        expect(result.required_rng_sources).to eql([])
+        expect(result.switch_type).to eql(SDK::SwitchType::LEGACY)
+      end
+
+    end
+
+  end
+
+end
