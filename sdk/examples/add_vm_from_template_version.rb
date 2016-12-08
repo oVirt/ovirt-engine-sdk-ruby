65a5e89f15d30363c66c3a9cf063c8f348b6b874
Juan Hernandez <juan.hernandez@redhat.com>
2016-12-08 13:40:01 +0100

    Add example of adding VM from template version
    This patch adds an example that shows how to add a virtual machine from
    an explictly indicated version of a template.
    
    Change-Id: Ic4ab43edf43acdfffc227a1d8f653d3ee3bf7b01
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 86d5978d750af2fee50aee6989c59ccf83634847)

---
+++ sdk/examples/add_vm_from_template_version.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/ruby
+
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'logger'
+require 'ovirtsdk4'
+
+# This example will connect to the server, and create a virtual machine
+# from a specific version of a template.
+
+# Create the connection to the server:
+connection = OvirtSDK4::Connection.new(
+  url: 'https://engine40.example.com/ovirt-engine/api',
+  username: 'admin@internal',
+  password: 'redhat123',
+  ca_file: 'ca.pem',
+  debug: true,
+  log: Logger.new('example.log')
+)
+
+# Get the reference to the root of the tree of services:
+system_service = connection.system_service
+
+# Get the reference to the service that manages the templates:
+templates_service = system_service.templates_service
+
+# When a template has multiple versions they all have the same name, so
+# we need to explicitly find the one that has the version name or
+# version number that we want to use. In this case we want to use
+# version 3 of the template.
+templates = templates_service.list(search: 'name=mytemplate')
+template = templates.find { |x| x.version.version_number == 3 }
+template_id = template.id
+
+# Get the reference to the service that manages the virtual machines:
+vms_service = system_service.vms_service
+
+# Add a new virtual machine explicitly indicating the identifier of the
+# template version that we want to use:
+vm = vms_service.add(
+  OvirtSDK4::Vm.new(
+    name: 'myvm',
+    cluster: {
+      name: 'mycluster'
+    },
+    template: {
+      id: template_id
+    }
+  )
+)
+
+# Get a reference to the service that manages the virtual machine that
+# was created in the previous step:
+vm_service = vms_service.vm_service(vm.id)
+
+# Wait till the virtual machine is down, which indicats that all the
+# disks have been created:
+loop do
+  sleep(5)
+  vm = vm_service.get
+  break if vm.status == OvirtSDK4::VmStatus::DOWN
+end
+
+# Close the connection to the server:
+connection.close
