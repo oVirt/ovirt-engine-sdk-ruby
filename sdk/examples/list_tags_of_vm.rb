8f7a3fd671b4e9ed9ae9980361e27d7353cb57bf
Ondra Machacek <omachace@redhat.com>
2016-12-12 17:46:18 +0100

    Add examples of how to work with tags
    This patch adds an examples of how to work with tags.
    
    Change-Id: Ie480fdd05ddd7e11dddb7bd487d193e16734a440
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit 039db24fd75aea377b46a5e833425b9d99a3e5c0)

---
+++ sdk/examples/list_tags_of_vm.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/ruby
+
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'logger'
+require 'ovirtsdk4'
+
+# This example will connect to the server and list all tags of the virtual machine:
+
+# Create the connection to the server:
+connection = OvirtSDK4::Connection.new(
+  url: 'https://engine40.example.com/ovirt-engine/api',
+  username: 'admin@internal',
+  password: 'redhat123',
+  ca_file: 'ca.pem',
+  debug: true,
+  log: Logger.new('example.log')
+)
+
+# Get the reference to the "vms" service:
+vms_service = connection.system_service.vms_service
+
+# Find the virtual machine:
+vm = vms_service.list(search: 'name=myvm0').first
+
+# Find the service that manages the vm:
+vm_service = vms_service.vm_service(vm.id)
+
+# Locate the service that manages the tags of the vm:
+tags_service = vm_service.tags_service
+
+# Print all name and description of tags assigned to virtual machine:
+tags_service.list.each do |tag|
+  puts "#{tag.name}: #{tag.description}"
+end
+
+# Close the connection to the server:
+connection.close
