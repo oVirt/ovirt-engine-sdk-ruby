4d3cb860592548e2805f530f8e9cb8343284ab4c
Juan Hernandez <juan.hernandez@redhat.com>
2017-03-03 10:28:50 +0100

    Don't avoid reserved words in enum values text
    The SDK appends an underscore to generated names that conflict with Ruby
    reserved words. But it also does so for the text of enum values. This is
    wrong, in particular it is wrong for the 'InheritableBoolean' enum which
    has the following values currently:
    
      FALSE = 'false_'.freeze
      TRUE = 'true_'.freeze
      ...
    
    The 'false_' and 'true_' values are incorrect, as that isn't what the
    server generates or expects, they should be 'false' and 'true', without
    the underscore.
    
    This patch fixes that issue.
    
    Change-Id: I5141e2be962d123934a35d14349d685f14e3bcd8
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ sdk/spec/inheritable_boolean_spec.rb
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+describe SDK::InheritableBoolean do
+  it 'uses the `true`  as the value for the `TRUE` constant' do
+    expect(SDK::InheritableBoolean::TRUE).to eql('true')
+  end
+
+  it 'uses the `false` as the value for the `FALSE` constant' do
+    expect(SDK::InheritableBoolean::FALSE).to eql('false')
+  end
+end
