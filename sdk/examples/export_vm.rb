40520fc6588a7105b0cfa7d4d0a8e76ce5fc944d
Juan Hernandez <juan.hernandez@redhat.com>
2017-04-07 17:49:52 +0200

    Add export VM example
    This patch adds an example that shows how to export a virtual machine to
    an export storage domain.
    
    Change-Id: Ia216de39527addd7ed0c10b43d704e1c1bb40fee
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 408dfbda51765b4f65af73ddc959edef9bb0ad96)

---
+++ sdk/examples/export_vm.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/ruby
+
+#
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'logger'
+require 'ovirtsdk4'
+
+# This example shows how export a virtual machine to an export storage domain.
+
+# Create the connection to the server:
+connection = OvirtSDK4::Connection.new(
+  url: 'https://engine40.example.com/ovirt-engine/api',
+  username: 'admin@internal',
+  password: 'redhat123',
+  ca_file: 'ca.pem',
+  debug: true,
+  log: Logger.new('example.log')
+)
+
+# Get the reference to the root of the services tree:
+system_service = connection.system_service
+
+# Find the virtual machine:
+vms_service = system_service.vms_service
+vm = vms_service.list(search: 'name=myvm').first
+
+# Export the virtual machine. Note that the 'exclusive' parameter is optional, and only required if you want to
+# overwrite a virtual machine that has already been exported before.
+vm_service = vms_service.vm_service(vm.id)
+vm_service.export(
+  exclusive: true,
+  discard_snapshots: true,
+  storage_domain: {
+    name: 'myexport'
+  }
+)
+
+# Close the connection to the server:
+connection.close
