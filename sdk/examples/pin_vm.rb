5326a213617690b13253184cbf351b4eb434d9f6
Juan Hernandez <juan.hernandez@redhat.com>
2017-04-19 10:02:13 +0200

    Add pin VM example
    This patch adds an example that shows how to modify the placement policy
    of a virtual machine so that it will always run in an specific host.
    
    Change-Id: I386776ead5afddf49b333b01944cb5e36cfa7834
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ sdk/examples/pin_vm.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+
+#
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'logger'
+require 'ovirtsdk4'
+
+# This example shows how to pin a virtual machine to a host, so that it will always run in that host.
+
+# Create the connection to the server:
+connection = OvirtSDK4::Connection.new(
+  url: 'https://engine40.example.com/ovirt-engine/api',
+  username: 'admin@internal',
+  password: 'redhat123',
+  ca_file: 'ca.pem',
+  debug: true,
+  log: Logger.new('example.log')
+)
+
+# Get the reference to the root of the tree of services:
+system_service = connection.system_service
+
+# Find the virtual machine:
+vms_service = system_service.vms_service
+vm = vms_service.list(search: 'name=myvm').first
+
+# Update the placement policy of the virtual machine so that it is pinned to the host.
+vm_service = vms_service.vm_service(vm.id)
+vm_service.update(
+  OvirtSDK4::Vm.new(
+    placement_policy: {
+      hosts: [
+        {
+          name: 'myhost'
+        }
+      ]
+    }
+  )
+)
+
+# Close the connection to the server:
+connection.close
